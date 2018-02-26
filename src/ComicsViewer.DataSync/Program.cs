e14fec77d208f716a6b637389aa5b4a337fc3320
branc116 <rickobranimir@gmail.com>
2018-02-27 00:05:26 +0100

    comics are now saved in common db. added nlog. added startup params.

--- src/ComicsViewer.DataSync/Program.cs
+++ src/ComicsViewer.DataSync/Program.cs
@@ -1,72 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ComicsViewer.Common.Repository;
+using ComicsViewer.Common.Resources;
 using HtmlAgilityPack;
 using Newtonsoft.Json;
-
-namespace Crawler.DataSync
+using NLog;
+using static ComicsViewer.DataSync.Helper.DataHelper;
+namespace ComicsViewer.DataSync
 {
-    class Program
+    public class Program
     {
+        private readonly static Logger _logger = LogManager.GetCurrentClassLogger();
+        private readonly static ComicRepository _comicRepository = GetRepository();
+        private readonly static HtmlWeb web = new HtmlWeb();
+        private readonly static Comic masterComic = _comicRepository.GetComic("MasterComic");
         static void Main(string[] args)
         {
-            List<Issue> comics = new List<Issue>();
-
-            HtmlWeb web = new HtmlWeb();
-            for (int i = 1; i < 50; i++)
+            int skip = 0;
+            int take = 2112;
+            if (args.Length > 0) 
+                skip = int.TryParse(args[0], out int arg1) ? arg1 : skip;
+            if (args.Length > 1)
+                take = int.TryParse(args[1], out int arg1) ? arg1 : take;
+            _logger.Trace($"START skip: {skip} take: {take}");
+            foreach (var page in ComicViewerPages().Skip(skip).Take(take))
             {
-                HtmlDocument doc = web.Load("http://viewcomic.com/page/" + i.ToString());
-
-                var nodes = doc.DocumentNode.SelectNodes("//a[@class='front-link']");
-
-                foreach (var item in nodes)
+                var hasPassed = false;
+                HtmlDocument doc;
+                List<Issue> issues;
+                HtmlNodeCollection nodes;
+                while (!hasPassed)
                 {
-                    var name = item.InnerText.Split('…').First().Trim();
-                    var links = pictureLinks(item.Attributes["href"].Value);
-                    var newIssue = new Issue
+                    try
                     {
-                        Name = name,
-                        Links = links
-                    };
-                    comics.Add(newIssue);
+                        doc = web.Load(page);
+                        issues = new List<Issue>();
+                        nodes = doc.DocumentNode.SelectNodes("//a[@class='front-link']");
+
+                        if (nodes.Count != 20)
+                        {
+                            _logger.Warn($"node count not 20 {doc.DocumentNode.InnerHtml}");
+                        }
+                        foreach (var item in nodes)
+                        {
+                            var name = item.InnerText.Split('…').First().Trim();
+                            if (!_comicRepository.Contains(name))
+                            {
+                                var links = pictureLinks(item.Attributes["href"].Value);
+                                if (!links.Any())
+                                    continue;
+
+                                var issue = new Issue
+                                {
+                                    Comic = masterComic,
+                                    IssueNumber = name
+                                };
+                                issue.IssuePictureLinks = links.Select(i => new IssuePicture
+                                {
+                                    Issue = issue,
+                                    Url = i
+                                }).ToList();
+                                issues.Add(issue);
+                            }
+                        }
+                        _comicRepository.AddNewIssues(issues, false);
+                        _logger.Debug("Done:" + page);
+                        hasPassed = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Error(ex);
+                        System.Threading.Thread.Sleep(1000);
+                    }
                 }
-                Console.WriteLine("Done:" + i.ToString());
             }
-            Console.WriteLine("Done");
-            System.IO.File.WriteAllText(@"C:\Users\Nikola\Desktop\comic.json", JsonConvert.SerializeObject(comics, Formatting.Indented));
-            Console.ReadKey();
+            _logger.Trace("END");
         }
 
         static List<string> pictureLinks(string uri)
         {
-            HtmlWeb web = new HtmlWeb();
-
+            
             var doc = web.Load(uri);
+            try
+            {
+                List<string> returnLinks = new List<string>();
+                var firstSelector = doc.DocumentNode.SelectNodes("//div[@class='separator']");
+                if (firstSelector != null && firstSelector.Any())
+                {
+                    returnLinks = doc.DocumentNode.SelectNodes("//div[@class='separator']").Descendants("a")
+                        .Select(i => i.Attributes["href"].Value).ToList();
+                }else
+                {
+                    var selector2 = doc.DocumentNode.SelectNodes("//img[@class='picture aligncenter']");
+                    if (selector2 != null) {
+                        returnLinks = selector2.Select(i => i.Attributes["src"].Value).ToList();
+                    }else
+                    {
+                        _logger.Warn($"No links on uri: {uri}");
+                    }
 
-            var links = doc.DocumentNode.SelectNodes("//div[@class='separator']").Descendants("a")
-                .Select(i => i.Attributes["href"].Value).ToList();
-            return links;
-
-
+                }
+                return returnLinks;
+            }catch(Exception ex)
+            {
+                _logger.Error(ex);
+                throw;
+            }
         }
     }
-
-    public class Issue
-    {
-        public string Name { get; set; }
-        public List<string> Links { get; set; }
-    }
-
-    public class Comic
-    {
-        public string Name { get; set; }
-        public List<Issue> Issues { get; set; }
-        public int Count { get; set; } = 0;
-
-        public Comic(string name)
-        {
-            Name = name;
-        }
-    }
-
 }
